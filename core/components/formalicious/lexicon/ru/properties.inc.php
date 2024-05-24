e28ac3e1528c08d87a0e22391aed3937a9bf7e4a
Anton Tarasov <himurovich@gmail.com>
2024-05-24 20:55:12 +0400

    Update properties.inc.php
    Russian translation

--- core/components/formalicious/lexicon/ru/properties.inc.php
+++ core/components/formalicious/lexicon/ru/properties.inc.php
@@ -8,11 +8,11 @@
 
 $_lang['formalicious']                                          = 'Formalicious';
 
-$_lang['formalicious.snippet_tplform_desc']                     = 'The template of the form, this can be a chunk name, @FILE or @INLINE.';
-$_lang['formalicious.snippet_tplstep_desc']                     = 'The template of a step, this can be a chunk name, @FILE or @INLINE.';
-$_lang['formalicious.snippet_tplnavigationitem_desc']           = 'The template of a step in the steps navigation, this can be a chunk name, @FILE or @INLINE.';
-$_lang['formalicious.snippet_tplnavigationwrapper_desc']        = 'The template of the steps navigation, this can be a chunk name, @FILE or @INLINE.';
-$_lang['formalicious.snippet_tplemail_desc']                    = 'The template of the email, this can only be a chunk name.';
-$_lang['formalicious.snippet_tplemailfieldsitem_desc']          = 'The template of a field in the email, this can be a chunk name, @FILE or @INLINE.';
-$_lang['formalicious.snippet_tplemailfieldswrapper_desc']       = 'The template of the wrapper of the fields in the email, this can be a chunk name, @FILE or @INLINE.';
-$_lang['formalicious.snippet_tplfiaremail_desc']                = 'The template of the auto-reply email, this can only be a chunk name.';
+$_lang['formalicious.snippet_tplform_desc']                     = 'Шаблон формы, это может быть имя чанка, @FILE или @INLINE.';
+$_lang['formalicious.snippet_tplstep_desc']                     = 'Шаблон шага формы, это может быть имя чанка, @FILE или @INLINE.';
+$_lang['formalicious.snippet_tplnavigationitem_desc']           = 'Шаблон шага формы в навигации по шагам, это может быть имя чанка, @FILE или @INLINE.';
+$_lang['formalicious.snippet_tplnavigationwrapper_desc']        = 'Шаблон-обертка навигации по шагам, это может быть имя чанка, @FILE или @INLINE.';
+$_lang['formalicious.snippet_tplemail_desc']                    = 'Шаблон письма, это может быть только имя чанка.';
+$_lang['formalicious.snippet_tplemailfieldsitem_desc']          = 'Шаблон поля в письме, это может быть имя чанка, @FILE или @INLINE.';
+$_lang['formalicious.snippet_tplemailfieldswrapper_desc']       = 'Шаблон обертки полей в письме,  это может быть имя чанка, @FILE или @INLINE.';
+$_lang['formalicious.snippet_tplfiaremail_desc']                = 'Шаблон письма авто-ответа, это может быть только имя чанка.';
